b31576a4931828fad0ea754d4846b5abdc69cc4a
david <david@hamlyn>
2023-01-16 13:58:54 +0000

    Wide help information

--- PurpleDevTools/Public/New-TypeFormatData.ps1
+++ PurpleDevTools/Public/New-TypeFormatData.ps1
@@ -8,13 +8,18 @@
 
     Creates the file myfile.ps1xml if it does not exist and populates it with the specified XML view. In the example, this is a table view with 3 properties for the class MyClass.
 
+.EXAMPLE
+    New-ClassFormatData -Path myfile.ps1xml -TypeName MyClass -View Wide -Properties Prop1 -ViewOptions @{WideColumns = 4}
+
+    Creates the file myfile.ps1xml if it does not exist and populates it with the specified XML view. In the example, this is a Wide view with a single property for the class MyClass. It will alwasy show 4 objects per row.
+
 .PARAMETER TypeName
 
     Specify the TypeName(s) for this view, if more that one type is spcified, then a single view is created to target all types.
 
 .PARAMETER View
 
-    Specify the Formatting view that will be used. Table or List.
+    Specify the Formatting view that will be used. Table, List or Wide.
 
 .PARAMETER Properties
 
@@ -30,6 +35,9 @@
     * Width        : The width in charaters for the specified Column (Table Only.)
     * Alignment    : The alignment of the Value in the specified Column, eg Left, Right (Table Only.)
     * Condition    : A script block to determine if the List property should be shown, return true to show the property (List Only.)
+    * Format       : Formats the property value using the given format string. (Wide Only.)
+
+    If the View type was set to Wide, then only first property is used. If you want multiple properties in a Wide view, use a script block to produce the string you want to see.
 
 .PARAMETER Group
 
@@ -46,6 +54,13 @@
     Path to save the View to. If an existing file is specified, then the view will be added to the existing xml tree in the file. If it does not exist a new file will be created.
 
     If not specified, then an XML document will instead by returned with the view added.
+
+.PARAMETER ViewOptions
+
+    Specify view options that apply to the whole view not just a single propertry of an object. Current options are:
+
+    * [string] NoAutoSize    : Disables autosize for Wide view, if specified the formatting engine uses only the first row for sizing info instead of all items.
+    * [property] Widecolumns : Sets the number of columns for Wide view. This also disables autosize, but allows display after the first row of items.
 
 #>
 function New-TypeFormatData {
