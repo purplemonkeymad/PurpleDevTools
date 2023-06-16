88ffc06e5e3b7aaf494a334cc30b3750b06e0b99
david <david@hamlyn>
2023-06-16 20:31:58 +0100

    Comment help for object list template.

--- PurpleDevTools/Public/New-ObjectListCommand.ps1
+++ PurpleDevTools/Public/New-ObjectListCommand.ps1
@@ -1,17 +1,45 @@
 <#
-.SYNOPSIS
-    Create a set of command to manage a saved list for a set class.
-.DESCRIPTION
-    Takes a class file/name and creates get/add/set/new/remove verbs for the class. The list will be saved to either a 
-    Specified path or a variable name for a path.
-.NOTES
-    
-.LINK
-    
-.EXAMPLE
-    New-ObjectListCommand -Class [test] -Path .\ -ListPath '$env:programdata\test.json'
-    Uses the loaded type test as the base of the command templates. Function files will be saved to '.\',
-    and the list save location will be '$env:programdata\test.json'.
+
+.Synopsis
+Create a set of command to manage a saved list for a set class.
+
+.Description
+Takes a class file/name and creates get/add/set/new/remove verbs for the class. The list will be saved to either a 
+Specified path or a variable name for a path.
+
+.Parameter Type
+Class to use as the base of the template. This can either be the type object or the full name of the type as a
+string.
+
+Supported classes should have a 0 argument constructor or support casting from a hashtable. ie [type]@{property=value}
+
+.Parameter Path
+A folder path for the output files of the template.
+
+
+.Parameter ListPath
+A code snippit, or script block to get the file used to serialize the list of objects. If you supply a path
+you should wrap it in quotes ie: {"$env:programdata\example.clixml"}
+
+.Parameter Noun
+Use a custom Noun for the command names, if unspecified the type's base name will be used instead.
+
+
+.Parameter IdentityPropertyName
+Specify which property is used as the identity. It will be treated as unique and will be required on
+any parameters.
+
+If this is unspecified, command that require a unique identity to change an object will not be generated.
+ie remove- and set- will not exist if unspecified.
+
+
+.Example
+New-ObjectListCommand -Class [test] -Path .\ -ListPath '$env:programdata\test.json'
+Uses the loaded type test as the base of the command templates. Function files will be saved to '.\',
+and the list save location will be '$env:programdata\test.json'.
+
+.Links
+
 #>
 function New-ObjectListCommand {
     [CmdletBinding()]
