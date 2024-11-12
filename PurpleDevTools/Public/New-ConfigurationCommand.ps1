e914daa0532e09b8bcc2e36f5f8733a340120c5d
david <david@hamlyn>
2024-11-12 12:06:46 +0000

    Add: Comment based help for config command

--- PurpleDevTools/Public/New-ConfigurationCommand.ps1
+++ PurpleDevTools/Public/New-ConfigurationCommand.ps1
@@ -1,4 +1,54 @@
 function New-ConfigurationCommand {
+    <#
+
+    .Synopsis
+    Create new commands to save and update settings based on a class.
+
+    .Description
+    Creates new commands that will save, update, and load configuration values.
+
+    The command is based around a single type, it's properties are used as the possible configuration values.
+    It's best to use a quite basic type, a constructor with 0 arguments is required for the configuration.
+
+    The type definition is not included by the commands, so make sure your module imports/creates it.
+
+    .Parameter Noun
+    Noun to use for new commands (part after the dash [-].)
+
+    If not specified, will use the type's short name.
+
+    .Parameter Path
+    Output path for the function files, one for each command.
+
+    .Parameter SettingPath
+    Path to store the configuration data in. By default uses roaming appdata & the non.
+
+    You can specify either a string (which will be used as is,) or you can provide
+    a scriptblock that should output the path. 
+
+    .Parameter Type
+    The type object of the type to use (or a string that will be recognised as a type.)
+
+    .Example
+    class MyConfiguration {
+        [string]$Identity
+        [int]$Value
+    }
+    PS > New-ConfigurationCommand -Type [MyConfiguration] -Path ./
+
+    Will create the commands in the current directory. The names will be Get-MyConfiguration
+    and Set-MyConfiguration. The default path to store the settings will be at
+    $env:appdata/MyConfiguration.xml. The Parameters on the set command will be
+    Identity and Value.
+
+    .Example
+    New-ConfigurationCommand -Type [MyConfiguration] -Path ./ -Noun ModuleConfigruation -SettingsPath { [MyConfiguration]::GetPath($IsLinux) }
+
+    Will create the commands in the current directory. The names will be Get-ModuleConfigruation
+    and Set-ModuleConfigruation. The path to store the setting will be the result of the static
+    method on the configration object.
+
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
